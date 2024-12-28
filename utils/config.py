f7c9d1e8c6e7c9d8361603cf858095f8dd41513e
Opet321 <90963402+Opet321@users.noreply.github.com>
2024-12-28 17:58:18 +0700

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -10,7 +10,7 @@ bot_token = "7764544491:AAEuyGkR8DNvqLjktcb6CJlH2xpFKnENQpo"
 
 session_string = env.str("SESSION_STRING", "")
 
-db_url = env.str("MONGODB_URL", "")
+db_url = env.str("MONGODB_URL", "mongodb+srv://apem:apem@cluster0.iraog.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 db_name = "Dragon-Fork"
 
 version = "[Selfbot] Dragon-Fork v1.1.0"
